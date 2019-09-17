0d652e2772dc4455476e874069c82b6922804e05
Jake <jporetsky14@gmail.com>
2019-09-17 14:32:44 -0400

    added eclipse integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
 //============================================================================
-// Name        : SportsProgram.cpp
+// Name        : SportsProgram
 // Author      : 
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description :
 //============================================================================
 
 #include <iostream>
-using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
